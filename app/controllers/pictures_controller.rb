84ea8fe89ce469a967dbd5a050b4530014f10393
Abdus Samad Anwar <asa2107@gmail.com>
2017-04-28 09:43:36 -0500

    hi

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,5 +1,4 @@
-class PicturesController < ActionController::Base
-
+class PicturesController < ApplicationController
   def new_form
     render("pic_templates/new_form.html.erb")
   end
@@ -9,23 +8,22 @@ class PicturesController < ActionController::Base
   end
 
   def index
-      render("pic_templates/index.html.erb")
+    render("pic_templates/index.html.erb")
   end
 
   def show
-      render("pic_templates/show.html.erb")
+    render("pic_templates/show.html.erb")
   end
 
   def edit_form
-      render("pic_templates/edit_form.html.erb")
-  end
-
-  def destroy_row
-      render("pic_templates/destroy_row.html.erb")
+    render("pic_templates/edit_form.html.erb")
   end
 
   def update_row
-      render("pic_templates/update_row.html.erb")
+    render("pic_templates/update_row.html.erb")
   end
 
+  def destroy_row
+    render("pic_templates/destroy_row.html.erb")
+  end
 end
