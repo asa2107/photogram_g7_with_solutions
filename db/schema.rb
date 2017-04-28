0d0384d4becfe3c9e65de0a309d61ce055eb274d
Abdus Samad Anwar <asa2107@gmail.com>
2017-04-28 14:33:32 -0500

    commit 1

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170428140500) do
-
-  create_table "photos", force: :cascade do |t|
-    t.string   "source"
-    t.text     "caption"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
